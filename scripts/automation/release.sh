1dee7f35ee64676e800fa1dc4ddbe947dd7ff341
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-03-13 13:21:53 +0100

    Query gh pr checks

--- scripts/automation/release.sh
+++ scripts/automation/release.sh
@@ -2,15 +2,28 @@
 
 set -x
 
-script=$0
 function usage {
-    echo "Usage: $script --version string --base_branch string --github_token string "
+    echo "Usage: $0 --version string --base_branch string --github_token string "
     echo "Creates a new release"
     echo ""
     echo "  --version        string   Version of new release"
     echo "  --base_branch    string   Base branch"
     echo "  --github_token   string   Path to github token"
     echo ""
+}
+
+function die {
+    printf "Script failed: %s\n" "$1"
+    usage
+    exit 1
+}
+
+error() {
+  # store last exit code before invoking any other command
+  local exit_code="$?"
+  # print error message
+  echo Error: "$1"
+  exit $exit_code
 }
 
 while [ $# -gt 0 ]; do
@@ -25,17 +38,11 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-function die {
-    printf "Script failed: %s\n" "$1"
-    usage
-    exit 1
-}
-
-if [[ -z $version ]]; then
+if [[ -z ${version} ]]; then
     die "Missing parameter --version"
-elif [[ -z $base_branch ]]; then
+elif [[ -z ${base_branch} ]]; then
     die "Missing parameter --base_branch"
-elif [[ -z $github_token ]]; then
+elif [[ -z ${github_token} ]]; then
     die "Missing parameter --github_token"
 fi
 
@@ -59,27 +66,38 @@ git checkout -b ${release_branch} ${base_branch}
 # update version of python bindings
 python ${scripts_dir}/bump_mkpy_versions.py \
 --file ${python_version_file} \
---to_version 6.6.6 \
---to_backend_version 6.6.6
+--to_version ${version} \
+--to_backend_version ${version}
 git add ${python_version_file}
 git commit -m 'Update python bindings version'
 git push -u origin ${release_branch}
+
+# release has now diverged from the base branch, create a PR
+gh pr create \
+--base ${base_branch} \
+--head ${release_branch} \
+--title "Release ${tag}" \
+--fill
+
+gh pr checks ${release_branch} --watch || error "Checks failed"
 
 # update product version
 python ${scripts_dir}/bump_package_version.py \
 --nuspec_file ${nuspec_file} \
 --dir_build_props_file ${dir_build_props_file} \
 --version_tag "MeshKernelReleaseAutomationVersion" \
---to_version 6.6.6
+--to_version ${version}
 git add ${nuspec_file} ${dir_build_props_file}
 git commit -m 'update product version'
 git push -u origin ${release_branch}
+
+gh pr checks ${release_branch} --watch || error "Checks failed"
 
 # update versions of dependencies
 python ${scripts_dir}/bump_dependencies_versions.py \
 --dir_packages_props_file ${dir_package_rops_file} \
 --to_versioned_packages \
-  "Deltares.MeshKernel:6.6.6.666-dev  \
+  "Deltares.MeshKernel:${version}  \
   Invalid:2666.09.13 \
   DHYDRO.SharedConfigurations:6.6.6.666 \
   NUnit:3.12.6"
@@ -87,12 +105,7 @@ git add ${dir_package_rops_file}
 git commit -m 'Update dependencies versions'
 git push -u origin ${release_branch}
 
-# release has now diverged from the base branch, cerate a PR
-gh pr create \
---base ${base_branch} \
---head ${release_branch} \
---title "Release ${tag}" \
---fill
+gh pr checks ${release_branch} --watch || error "Checks failed"
 
 # create tagged release from the release branch, set title same as tag, autogenerate the release notes and make set it to latest
 gh release create ${tag} \
