526597482f9af7f3fade2c15570af032f6a95b00
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-07-18 10:25:22 +0200

    Split utilities

--- scripts/automation/release.sh
+++ scripts/automation/release.sh
@@ -24,64 +24,11 @@ pypi_access_token=""
 teamcity_access_token=""
 clean=false
 
-source $(dirname $(realpath "$0"))/usage.sh
-source $(dirname $(realpath "$0"))/monitor_checks_on_branch.sh
-
-function col_echo() {
-    local color=$1
-    local text=$2
-    if ! [[ ${color} =~ '^[0-9]$' ]]; then
-        case $(echo ${color} | tr '[:upper:]' '[:lower:]') in
-        --black | -k)
-            color=0
-            ;;
-        --red | -r)
-            color=1
-            ;;
-        --green | -g)
-            color=2
-            ;;
-        --yellow | -y)
-            color=3
-            ;;
-        --blue | -b)
-            color=4
-            ;;
-        --magenta | -m)
-            color=5
-            ;;
-        --cyan | -c)
-            color=6
-            ;;
-        --white | -w)
-            color=7
-            ;;
-        *) # default color
-            color=9
-            ;;
-        esac
-    fi
-    tput setaf ${color}
-    echo ${text}
-    tput sgr0
-}
-
-function catch() {
-    local exit_code=$1
-    if [ ${exit_code} != "0" ]; then
-        col_echo --red "Error occurred"
-        col_echo --red "  Line     : ${BASH_LINENO[1]}"
-        col_echo --red "  Function : ${FUNCNAME[1]}"
-        col_echo --red "  Command  : ${BASH_COMMAND}"
-        col_echo --red "  Exit code: ${exit_code}"
-    fi
-}
-
+source $(dirname $(realpath "$0"))/utilities.sh
 trap 'catch $?' EXIT
 
-function show_progress() {
-    col_echo --blue ">> ${FUNCNAME[1]}"
-}
+source $(dirname $(realpath "$0"))/usage.sh
+source $(dirname $(realpath "$0"))/monitor_checks_on_branch.sh
 
 # Define the parse_named_arguments function
 function parse_arguments() {
